1530a247c3f71840afdcb1706942658c69ce1ab0
LandenTy <93301002+LandenTy@users.noreply.github.com>
2022-07-29 13:44:29 -0500

    Add files via upload
    Version 0.2 of my PS3 GUI System! This system makes it 8x faster, as well as easier to work with!

---
+++ PS_GUI v0.2/ButtonController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonController : MonoBehaviour {
+
+	public Color32 notSelected;
+	public Color32 isSelected;
+	public Color32 isHovering;
+	public int id;
+
+	[Space(10)]
+	public UnityEvent onClickEvent;
+
+	[HideInInspector] public Image image;
+
+	void Start()
+	{
+		image = GetComponent<Image> ();
+		Init ();
+	}
+
+	private void Init()
+	{
+		if (id == 1)
+		{
+			image.color = isHovering;
+		}
+	}
+
+	public void OnButtonClicked(int id)
+	{
+		if (id == this.id)
+		{
+			image.color = isSelected;
+			onClickEvent.Invoke ();
+		}
+	}
+
+	public void OnButtonHovered(int id)
+	{
+		if (id == this.id)
+		{
+			image.color = isHovering;
+		}
+		else
+		{
+			image.color = notSelected;
+		}
+	}
+}
---
+++ PS_GUI v0.2/UIController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIController : MonoBehaviour {
+
+	public string upAxis;
+	public string downAxis;
+	public string confirmButton;
+
+	private bool isUp = false;
+	private bool isDown = false;
+
+	[HideInInspector] public int itemIndex = 0;
+	public List<ButtonController> buttonList = new List<ButtonController>();
+	public string currentlySelected;
+
+	void Start()
+	{
+		currentlySelected = buttonList [itemIndex].name;
+	}
+
+	void Update()
+	{
+		ItemSelection ();
+		ConfirmSelection ();
+	}
+
+	public void ItemSelection()
+	{
+		if ((!isUp) && (Input.GetAxisRaw(upAxis) == 1))
+		{
+			isUp = true;
+			this.CallWithDelay (LastItem, 0.2f);
+		}
+
+		if ((!isDown)  && (Input.GetAxisRaw(downAxis) == 1))
+		{
+			isDown = true;
+			this.CallWithDelay (NextItem, 0.2f);
+		}
+	}
+
+	public void LastItem()
+	{
+		if ((isUp) && (Input.GetAxisRaw(upAxis) == 0))
+		{
+			if (itemIndex > 0)
+			{
+				isUp = false;
+
+				itemIndex -= 1;
+				currentlySelected = buttonList [itemIndex].name;
+
+				SetButtonColor ();
+			}
+			else
+			{
+				isUp = false;
+			}
+		}
+	}
+
+	public void NextItem()
+	{
+		if ((isDown) && (Input.GetAxisRaw(downAxis) == 0))
+		{
+			if (itemIndex + 1 < buttonList.Count)
+			{
+				isDown = false;
+
+				itemIndex += 1;
+				currentlySelected = buttonList [itemIndex].name;
+
+				SetButtonColor ();
+			}
+			else
+			{
+				isDown = false;
+			}
+		}
+	}
+
+	public void ConfirmSelection()
+	{
+		if (Input.GetButtonUp(confirmButton))
+		{
+			buttonList [itemIndex].OnButtonClicked (itemIndex + 1);
+		}
+	}
+
+	public void SetButtonColor()
+	{
+		for (int i = 0; i < buttonList.Count; i++) {
+			buttonList [i].image.color = buttonList [i].notSelected;
+		}
+
+		buttonList [itemIndex].OnButtonHovered (itemIndex + 1);
+	}
+}
